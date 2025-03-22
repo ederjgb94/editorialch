0e58e9ca413c88fefb529794847786d52bb06a5b
“ederjgb94” <ederjbg94@gmail.com>
2025-03-22 14:01:40 -0600

    Organize API routes under versioning and add books resource routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+
+    Route::apiResource('books', 'App\Http\Controllers\BookController');
+});
