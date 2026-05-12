987bf63b9a47849b42ac0e6dd51041fcd4c045cd
ebravo-dev <ederjgb94@gmail.com>
2026-05-11 21:28:54 -0600

    Eliminar opción y rutas de registro público

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,6 @@ use App\Http\Controllers\AuthorController;
 Route::middleware('guest')->group(function () {
     Route::get('login',    [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login',   [AuthenticatedSessionController::class, 'store']);
-    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
-    Route::post('register',[RegisteredUserController::class, 'store']);
 });
 
 Route::middleware('auth')->group(function () {
